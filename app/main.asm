583f32138c97e9ea9bfce62883717a945725a013
kstopplecamp <49496979+kstopplecamp@users.noreply.github.com>
2025-01-20 11:45:29 -0700

    Delay loop working with a .5 HZ signal

--- app/main.asm
+++ app/main.asm
@@ -1,95 +1,60 @@
-; --COPYRIGHT--,BSD_EX
-;  Copyright (c) 2016, Texas Instruments Incorporated
-;  All rights reserved.
-;
-;  Redistribution and use in source and binary forms, with or without
-;  modification, are permitted provided that the following conditions
-;  are met:
-;
-;  *  Redistributions of source code must retain the above copyright
-;     notice, this list of conditions and the following disclaimer.
-;
-;  *  Redistributions in binary form must reproduce the above copyright
-;     notice, this list of conditions and the following disclaimer in the
-;     documentation and/or other materials provided with the distribution.
-;
-;  *  Neither the name of Texas Instruments Incorporated nor the names of
-;     its contributors may be used to endorse or promote products derived
-;     from this software without specific prior written permission.
-;
-;  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-;  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-;  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-;  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-;  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-;  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-;  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-;  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-;  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-;  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-;
-; ******************************************************************************
-;
-;                        MSP430 CODE EXAMPLE DISCLAIMER
-;
-;  MSP430 code examples are self-contained low-level programs that typically
-;  demonstrate a single peripheral function or device feature in a highly
-;  concise manner. For this the code may rely on the device's power-on default
-;  register values and settings such as the clock configuration and care must
-;  be taken when combining code from several examples to avoid potential side
-;  effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
-;  for an API functional library-approach to peripheral configuration.
-;
-; --/COPYRIGHT--
-;******************************************************************************
-;  MSP430FR235x Demo - Toggle P1.0 using software
-;
-;  Description: Toggle P1.0 every 0.1s using software.
-;  By default, FR235x select XT1 as FLL reference.
-;  If XT1 is present, the PxSEL(XIN & XOUT) needs to configure.
-;  If XT1 is absent, switch to select REFO as FLL reference automatically.
-;  XT1 is considered to be absent in this example.
-;  ACLK = default REFO ~32768Hz, MCLK = SMCLK = default DCODIV ~1MHz.
-;
-;           MSP430FR2355
-;         ---------------
-;     /|\|               |
-;      | |               |
-;      --|RST            |
-;        |           P1.0|-->LED
-;
-;   Cash Hao
-;   Texas Instruments Inc.
-;   November 2016
-;   Built with Code Composer Studio v6.2.0
-;******************************************************************************
-            .cdecls C,LIST,"msp430.h"  ; Include device header file
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+; Kyle Stopplecamp, EELE 465, Project 1
+; Jan 16 2025
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
 ;-------------------------------------------------------------------------------
             .def    RESET                   ; Export program entry-point to
                                             ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+init:
+		bic.w	#BIT0, PM5CTL0  ; Disable the GPIO power-on default high-Z mode
+		bis.b   #BIT0, P1DIR		; SEt P1.0 as an output. P1.0 is LED1
+main:
+		xor.b	#01h, P1OUT 	; Toggle P1.0 (LED1)
+
+        mov.w	#1000, R5		    ; Put number into R4 for desired #ms delay 
+        add.w   #1, R5          ; Negates the first dec in time_delay
+reset:
+    	mov.w	#348, R4		; Put a number into R5 to make 1 ms
+time_delay:
+        dec.w	R5				; Decrement R5
+		jnz  	milsec			; Repeat until R4 is 0
+		jmp  	main			; Repeat main loop forever
+milsec:
+		dec.w	R4				; Decrement R4
+		jnz  	milsec			; Repeat until R4 is 0
+		jmp  	reset		    ; Repeat main loop forever
+        NOP
+
+
+                                            
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
             .global __STACK_END
-            .sect   .stack                  ; Make stack linker segment ?known?
-
-            .text                           ; Assemble to Flash memory
-            .retain                         ; Ensure current section gets linked
-            .retainrefs
-
-RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
-StopWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
-SetupP1     bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
-            bis.b   #BIT0,&P1DIR            ; P1.0 output
-            bic.w   #LOCKLPM5,&PM5CTL0       ; Unlock I/O pins
-
-Mainloop    xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.1s
-Wait        mov.w   #50000,R15              ; Delay to R15
-L1          dec.w   R15                     ; Decrement R15
-            jnz     L1                      ; Delay over?
-            jmp     Mainloop                ; Again
-            NOP
-;------------------------------------------------------------------------------
-;           Interrupt Vectors
-;------------------------------------------------------------------------------
-            .sect   RESET_VECTOR            ; MSP430 RESET Vector
-            .short  RESET                   ;
-            .end
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
